85aa73ef6aee0823d2a6aa6e86c19aab299a386d
guywithlightide <ksohams97@gmail.com>
2022-09-25 20:53:24 +0530

    first-commit

---
+++ Sorting/src/Runner.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Runner {
+	public static void main(String[] args) {
+		int [] originalArr = {78,34,1,23,9,0};
+		System.out.println("Before sorting => "+Arrays.toString(originalArr));
+		
+		int [] arr = Arrays.copyOf(originalArr, originalArr.length);
+		SelectionSort.sort(arr);
+		System.out.println("Selection sort => "+Arrays.toString(arr));
+		
+		arr = Arrays.copyOf(originalArr, originalArr.length);
+		BubbleSort.sort(arr);
+		System.out.println("Bubble sort => "+Arrays.toString(arr));
+			
+	}
+}
---
+++ Sorting/src/SelectionSort.java
@@ -0,0 +1,19 @@
+
+public class SelectionSort {
+	
+	public static int[] sort(int [] arr) {
+		for (int i = 0; i < arr.length-1; i++) {
+			int minIndex = i;
+			for (int j = i+1; j < arr.length; j++) {
+				if (arr[j] < arr[minIndex]) {
+					minIndex = j;
+				}
+			}
+			int temp = arr[minIndex];
+			arr[minIndex] = arr[i];
+			arr[i] = temp;
+		}
+		return arr;
+	}
+	
+}
